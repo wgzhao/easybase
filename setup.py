15dd517f1142d4d1806becf5e57e7d4043ce42b8
wgzhao <wgzhao@gmail.com>
2017-11-14 10:05:35 +0800

    upgrade to the  latest upload module policy

--- setup.py
+++ setup.py
@@ -23,22 +23,23 @@ def get_install_requires():
 setup(
     name='easybase',
     version=__version__,
-    description="""A developer-friendly Python library to interact with
-                Apache HBase,support thrift2. It's heavily references to happybase. thank Wouter Bolsterlee""",
+    description="""A developer-friendly Python library to interact with Apache HBase,support thrift2. It's heavily references to happybase. thank Wouter Bolsterlee""",
     long_description=get_file_contents('README.md'),
     author="wgzhao",
     author_email="wgzhao@gmail.com",
     url='https://github.com/wgzhao/easybase',
     install_requires=get_install_requires(),
+    keywords="HBase,easybase,happybase",
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     license="MIT",
-    classifiers=(
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
         "Topic :: Database",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    )
+    ]
 )
