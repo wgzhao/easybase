b0ff891b90db547340eac0b664f331c85222b4d2
wgzhao <wgzhao@gmail.com>
2019-09-11 15:02:47 +0800

    fix: xml file incorrect

--- travis_install.sh
+++ travis_install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -eux
 sudo apt-get update
-sudo apt-get -q install -y oracle-java8-installer python-dev 
+sudo apt-get -q install -y wget oracle-java8-installer python-dev 
 sudo update-java-alternatives -s java-8-oracle
 
 # download and install hbase 2.2.x
 ver='2.2.0'
 tarball="hbase-${ver}-bin.tar.gz"
-wget -O /tmp/${tarball} https://www-us.apache.org/dist/hbase/${ver}/${tarball}
+wget -q -O /tmp/${tarball} https://www-us.apache.org/dist/hbase/${ver}/${tarball}
 
 tar -xzf /tmp/${tarball} -C /var/tmp/
 
 #configuration
 cd /var/tmp/hbase-${ver}
 
-echo """
+cat <<EOF >conf/hbase-site.xml
 <?xml version="1.0"?>
 <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 <configuration>
@@ -29,7 +29,8 @@ echo """
     <name>hbase.unsafe.stream.capability.enforce</name>
     <value>false</value>
   </property>
-</configuration>""" >conf/hbase-site.xml
+</configuration>
+EOF
 
 export HBASE_HOME=/var/tmp/hbase-${ver}
 export PATH=$PATH:$HBASE_HOME/bin
