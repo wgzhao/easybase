3abe867e06e389d06751f28810fa2a1f3e8583c3
wgzhao <wgzhao@gmail.com>
2023-03-07 15:09:17 +0800

    satisfy pep8

--- easybase/__init__.py
+++ easybase/__init__.py
@@ -2,16 +2,11 @@
 EasyBase, a developer-friendly Python library to interact with Apache
 HBase. Support Time-Range scan and multi-version access
 """
-# using thriftpy2 server as thrift bridge directly 
+# using thriftpy2 server as thrift bridge directly
 from pkg_resources import resource_filename
 import thriftpy2
-from ._version import __version__
 
 thriftpy2.load(
     resource_filename('easybase', 'HBase.thrift'),
     module_name='HBase_thrift'
 )
-
-from .connection import DEFAULT_HOST, DEFAULT_PORT, Connection
-from .table import Table
-from .pool import ConnectionPool, NoConnectionsAvailable
