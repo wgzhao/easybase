5728d6bf920de18af09d25993c3631444b106f78
wgzhao <wgzhao@gmail.com>
2026-03-12 16:10:58 +0800

    Add support for Python 3.13 in setup.py classifiers

--- setup.py
+++ setup.py
@@ -39,6 +39,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Database",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ]
