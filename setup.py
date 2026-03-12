b6ce3041836134b7cba8f8afc3615af4ecd81230
wgzhao <wgzhao@gmail.com>
2026-03-12 15:15:56 +0800

    Remove support for Python 3.7 and 3.8, and add support for Python 3.11 and 3.12. Update the version to 2.0.0 and update the description in setup.py.

--- setup.py
+++ setup.py
@@ -39,10 +39,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         "Topic :: Database",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ]
